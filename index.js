b635610076f7327fce4f067c08bfcf42ee33920b
Jesus Escalante <jeemcars@hotmail.com>
2025-03-01 22:21:24 -0600

    se mejoro la logica del codigo

--- index.js
+++ index.js
@@ -1,71 +1,25 @@
-const li1 = document.querySelector('#li-1');
-const li2 = document.querySelector('#li-2');
-const li3 = document.querySelector('#li-3');
-const li4 = document.querySelector('#li-4');
-
-const div1 = document.querySelector('#div-1');
-const div2 = document.querySelector('#div-2');
-const div3 = document.querySelector('#div-3');
-const div4 = document.querySelector('#div-4');
-
-const lista = [li1, li2, li3, li4];
-
-const divs = [div1, div2, div3, div4];
+const lista = document.querySelectorAll('li');
+const divs = document.querySelectorAll('.main-div');
 
 console.log('Se requiere poner diseño a la pagina pipipipip');
 
 
 lista.forEach(li => {
     li.addEventListener('click', () => {
-        switch (li.id) {
-            case 'li-1':
-                div1.classList.add('visible');
-                div1.classList.remove('unvisible');
-                div2.classList.add('unvisible');
-                div3.classList.add('unvisible');
-                div4.classList.add('unvisible');
-                div2.classList.remove('visible');
-                div3.classList.remove('visible');
-                div4.classList.remove('visible');
-                break;
+        const listaSeleccionada = li.id;
+        console.log(listaSeleccionada)
+
+        divs.forEach(div => {
+            div.classList.remove('visible');
+            div.classList.add('unvisible');
             
-            case 'li-2':
-                div2.classList.add('visible');
-                div2.classList.remove('unvisible');
-                div1.classList.add('unvisible');
-                div3.classList.add('unvisible');
-                div4.classList.add('unvisible');
-                div1.classList.remove('visible');
-                div3.classList.remove('visible');
-                div4.classList.remove('visible');
-                break;
+            if(div.id == `div-${listaSeleccionada}`) {
+                console.log(div)
+                div.classList.add('visible');
+                div.classList.remove('unvisible');
 
-            case 'li-3':
-                div3.classList.add('visible');
-                div3.classList.remove('unvisible');
-                div1.classList.add('unvisible');
-                div2.classList.add('unvisible');
-                div4.classList.add('unvisible');
-                div1.classList.remove('visible');
-                div2.classList.remove('visible');
-                div4.classList.remove('visible');
-                break;
-
-            case 'li-4':
-                div4.classList.add('visible');
-                div4.classList.remove('unvisible');
-                div1.classList.add('unvisible');
-                div2.classList.add('unvisible');
-                div3.classList.add('unvisible');
-                div1.classList.remove('visible');
-                div2.classList.remove('visible');
-                div3.classList.remove('visible');
-                break;
-        
-            default:
-                break;
-        }
-        
+            }
+        });
     });
 });
 
